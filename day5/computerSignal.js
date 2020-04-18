66decfa5b881b39aa2b09e8903c20e8db1471d33
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-04-18 16:15:24 +0530

    Day_5: Part_2 Refactored part 2 seprated findOutPutSignal function make new short function like performOperation, increasePointer, getSubString, getPositions

--- day5/computerSignal.js
+++ day5/computerSignal.js
@@ -6,26 +6,27 @@ const addZero = function (number) {
   return extraZero + string;
 };
 
-const findOutputSignal = function (signals, ptrPos = 0) {
-  const req = addZero(signals[ptrPos]);
-  const opcode = +req.substr(3, 2);
-  const firstParamMode = +req.substr(2, 1);
-  const secondParamMode = +req.substr(1, 1);
-  const thirdParamMode = +req.substr(0, 1);
-  const pos1 = firstParamMode ? ptrPos + 1 : signals[ptrPos + 1];
-  const pos2 = secondParamMode ? ptrPos + 2 : signals[ptrPos + 2];
-  const pos3 = thirdParamMode ? ptrPos + 3 : signals[ptrPos + 3];
+const increment = function (value, incrementBy) {
+  return value + incrementBy;
+};
 
-  const increasePointer = {
-    1: () => (ptrPos += 4),
-    2: () => (ptrPos += 4),
-    3: () => (ptrPos += 2),
-    4: () => (ptrPos += 2),
-    5: () => (ptrPos = signals[pos1] ? signals[pos2] : (ptrPos += 3)),
-    6: () => (ptrPos = signals[pos1] ? (ptrPos += 3) : signals[pos2]),
-    7: () => (ptrPos += 4),
-    8: () => (ptrPos += 4),
+const increasePointer = function (positions, opcode, signals) {
+  let [pos1, pos2, ptrPos] = positions;
+  const pointerIncrement = {
+    1: () => increment(ptrPos, 4),
+    2: () => increment(ptrPos, 4),
+    3: () => increment(ptrPos, 2),
+    4: () => increment(ptrPos, 2),
+    5: () => (signals[pos1] ? signals[pos2] : increment(ptrPos, 3)),
+    6: () => (signals[pos1] ? increment(ptrPos, 3) : signals[pos2]),
+    7: () => increment(ptrPos, 4),
+    8: () => increment(ptrPos, 4),
   };
+  return pointerIncrement[opcode]();
+};
+
+const performOperations = function (positions, opcode, signals) {
+  const [pos1, pos2, pos3] = positions;
   const operations = {
     1: () => (signals[pos3] = signals[pos1] + signals[pos2]),
     2: () => (signals[pos3] = signals[pos1] * signals[pos2]),
@@ -36,11 +37,31 @@ const findOutputSignal = function (signals, ptrPos = 0) {
     7: () => (signals[pos3] = signals[pos1] < signals[pos2]),
     8: () => (signals[pos3] = signals[pos1] == signals[pos2]),
   };
+  operations[opcode]();
+  return signals;
+};
+
+const getSubstring = function (string, pos1, pos2) {
+  return string.substr(pos1, pos2);
+};
+
+const getPositions = function (signals, ptrPos, mode, pos) {
+  return mode ? ptrPos + pos : signals[ptrPos + pos];
+};
+
+const findOutputSignal = function (signals, ptrPos = 0) {
+  const req = addZero(signals[ptrPos]);
+  const opcode = +getSubstring(req, 3, 2);
+  const firstParamMode = +getSubstring(req, 2, 1);
+  const secondParamMode = +getSubstring(req, 1, 1);
+  const thirdParamMode = +getSubstring(req, 0, 1);
+  const pos1 = getPositions(signals, ptrPos, firstParamMode, 1);
+  const pos2 = getPositions(signals, ptrPos, secondParamMode, 2);
+  const pos3 = getPositions(signals, ptrPos, thirdParamMode, 3);
 
   if (opcode === 99) return signals[0];
-  operations[opcode]();
-  increasePointer[opcode]();
-
+  signals = performOperations([pos1, pos2, pos3], opcode, signals.slice());
+  ptrPos = increasePointer([pos1, pos2, ptrPos], opcode, signals.slice());
   return findOutputSignal(signals, ptrPos);
 };
 
