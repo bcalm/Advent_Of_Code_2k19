030f97ea2f97556cf1b804782b52a1fef9ab3cf7
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-04-24 10:34:06 +0530

    Day_9 Part_2 Change initial Input

--- day9/boost.js
+++ day9/boost.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const main = function () {
   const boostInput = JSON.parse(fs.readFileSync('./boostInput.json', 'utf8'));
   const input = new IntCode(boostInput);
-  const boost = runIntCode(input, [2]);
+  const boost = runIntCode(input, [1]);
   console.log(boost);
 };
 
