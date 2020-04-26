ce23152ea08edd9e4814933d037037ad3a4de6dc
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-04-26 16:38:43 +0530

    Day_14 Part_1 Completed Used recursive method for calculating orecount. Took one inputChemical and go until find the ORE. Removed extra fuel also from total count each time.

--- day14/findFuel.js
+++ day14/findFuel.js
@@ -1,92 +1,46 @@
 const fs = require('fs');
 
-const getReagent = function (reagentList) {
-  const reagents = [];
-  reagentList.split(', ').forEach((reagent) => {
-    reagents.push(reagent.split(' '));
+const getReagents = function (inputChemicals) {
+  const reagent = [];
+  inputChemicals.split(', ').forEach((inputChemical) => {
+    reagent.push(inputChemical.split(' '));
   });
-  return reagents;
+  return reagent;
 };
 
-const getReactions = function (reactions) {
-  reactions = reactions.split('\n').map((reaction) => reaction.split(' => '));
-  return reactions.map((reaction) => {
-    const reactionDetails = {};
-    reactionDetails.reagent = getReagent(reaction[0]);
-    reactionDetails.product = reaction[1].split(' ');
-    return reactionDetails;
+const parseReactions = function (reactions, reactionInput) {
+  const [inputChemicals, product] = reactionInput.split(' => ');
+  const reagents = getReagents(inputChemicals);
+  const [count, productName] = product.split(' ');
+  reactions[productName] = {count: +count, inputs: reagents};
+  return reactions;
+};
+
+const getOreCount = function (reactions, inputChemical, ore = 1, remaining = {}) {
+  if (inputChemical === 'ORE') return ore;
+
+  remaining[inputChemical] = remaining[inputChemical] ? remaining[inputChemical] : 0;
+  const extra = Math.min(ore, remaining[inputChemical]);
+  ore -= extra;
+  remaining[inputChemical] -= extra;
+
+  const {count, inputs} = reactions[inputChemical];
+  let oreCount = 0;
+
+  inputs.forEach((input) => {
+    const [reagentCount, chemical] = input;
+    const multiple = Math.ceil(ore / +count) * +reagentCount;
+    oreCount += getOreCount(reactions, chemical, multiple, remaining);
   });
-};
 
-const getInputChemicals = function (reactions, outputChemical) {
-  reactions = JSON.parse(JSON.stringify(reactions));
-  if (outputChemical[1] === 'ORE') return [outputChemical];
-  const correctReaction = reactions.find((reaction) => reaction.product[1] == outputChemical[1]);
-  if (correctReaction && correctReaction.reagent[0][1] === 'ORE') return [outputChemical];
-  const productCount = +correctReaction.product[0];
-  correctReaction.reagent = correctReaction.reagent.map((reagent) => {
-    return [+reagent[0] * Math.ceil(+outputChemical[0] / productCount), reagent[1]];
-  });
-  return correctReaction.reagent;
-};
-
-const isAllOre = function (fuelReagent, reactions) {
-  const oreProduct = reactions.map((reaction) => {
-    if (reaction.reagent[0][1] == 'ORE') {
-      return reaction.product[1];
-    }
-  });
-  return (
-    fuelReagent.filter((reagent) => oreProduct.includes(reagent[1])).length == fuelReagent.length
-  );
-};
-
-const getOreProduct = function (reactions, fuelReagent) {
-  if (isAllOre(fuelReagent, reactions)) return fuelReagent;
-
-  const inputChemicals = fuelReagent
-    .map((reagent) => getInputChemicals(reactions, reagent))
-    .flat(1);
-  return getOreProduct(reactions, inputChemicals);
-};
-
-const isExists = function (product, uniqProduct) {
-  return uniqProduct.filter((element) => element[1] === product).length;
-};
-
-const getUniqProduct = function (oreProduct) {
-  return oreProduct.reduce((uniqProducts, product) => {
-    if (isExists(product[1], uniqProducts)) {
-      const index = uniqProducts.findIndex((element) => element[1] === product[1]);
-      uniqProducts[index][0] += product[0];
-    } else {
-      uniqProducts.push(product);
-    }
-    return uniqProducts;
-  }, []);
-};
-
-const getOreCount = function (product, reactions) {
-  const count = product.reduce((count, newProduct) => {
-    const relation = reactions.find((elem) => elem.product[1] === newProduct[1]);
-    const productMade = Math.ceil(newProduct[0] / +relation.product[0]);
-    return count + relation.reagent[0][0] * productMade;
-  }, 0);
-  return count;
-};
-
-const calculateOREForFuel = function (reactions) {
-  const fuelReagent = getInputChemicals(reactions, ['1', 'FUEL']);
-  const oreProduct = getOreProduct(reactions, fuelReagent);
-  const uniqProduct = getUniqProduct(oreProduct);
-  const oreCount = getOreCount(uniqProduct, reactions);
-  console.log(oreCount);
+  remaining[inputChemical] += Math.ceil(ore / count) * count - ore;
+  return oreCount;
 };
 
 const main = function () {
-  const reactionInput = fs.readFileSync('reaction.json', 'utf8');
-  const reactions = getReactions(reactionInput);
-  const oreCount = calculateOREForFuel(reactions);
+  const reactionInputs = fs.readFileSync('reaction.json', 'utf8');
+  const reactions = reactionInputs.split('\n').reduce(parseReactions, {});
+  console.log(getOreCount(reactions, 'FUEL'));
 };
 
 main();
