400a73171b4ee321067c906a05d32f7ea3d14448
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-04-29 15:57:42 +0530

    Day_16 Part_1 Refactored

--- day16/gasGiant.js
+++ day16/gasGiant.js
@@ -1,48 +1,47 @@
 const fs = require('fs');
 
-const getAllPattern = function (inputLength) {
-  const basePattern = ['0', '1', '0', '-1'];
-  const patterns = [];
-  for (let row = 0; row < inputLength; row++) {
-    patternSetting = [];
-    let pattern = '';
-    for (let index = 0; index < basePattern.length; index++) {
-      pattern = pattern.concat(basePattern[index].repeat(row + 1));
-    }
-    pattern = pattern.repeat(inputLength);
-    for (let index = 0; index < pattern.length; index++) {
-      if (pattern[index] == '-') {
-        patternSetting.push(pattern.substr(index, 2));
-        index++;
-      } else {
-        patternSetting.push(pattern[index]);
-      }
-    }
-    patterns.push(patternSetting.slice(1, inputLength + 1));
+const getAllPattern = function (inputLength, basePattern, patterns = [], row = 0) {
+  if (row === inputLength) return patterns;
+  const patternSetting = [];
+  let pattern = '';
+  for (let index = 0; index < basePattern.length; index++) {
+    pattern = pattern.concat(basePattern[index].repeat(row + 1));
   }
-  return patterns;
+  pattern = pattern.repeat(inputLength);
+
+  for (let index = 0; index < pattern.length; index++) {
+    if (pattern[index] == '-') {
+      patternSetting.push(pattern.substr(index, 2));
+      index++;
+    } else {
+      patternSetting.push(pattern[index]);
+    }
+  }
+  patterns.push(patternSetting.slice(1, inputLength + 1));
+
+  return getAllPattern(inputLength, basePattern, patterns, ++row);
 };
 
-const getPhaseOutput = function (fftInput) {
-  const patterns = getAllPattern(fftInput.length);
-  for (let phase = 0; phase < 100; phase++) {
-    const newInput = [];
-    for (let i = 0; i < fftInput.length; i++) {
-      let input = 0;
-      for (let j = 0; j < fftInput.length; j++) {
-        input += fftInput[j] * patterns[i][j];
-      }
-      newInput.push(Math.abs(input.toString()[input.toString().length - 1]));
+const getPhaseOutput = function (fftInput, patterns, phase = 0) {
+  if (phase === 4) return fftInput.slice(0, 8);
+
+  const newInput = [];
+  for (let row = 0; row < fftInput.length; row++) {
+    let input = 0;
+    for (let col = 0; col < fftInput.length; col++) {
+      input += fftInput[col] * patterns[row][col];
     }
-    fftInput = newInput.join('');
+    newInput.push(Math.abs(input % 10));
   }
-  return fftInput.slice(0, 8);
+  return getPhaseOutput(newInput.join(''), patterns, ++phase);
 };
 
 const main = function () {
   const input = fs.readFileSync('./fftInput.json', 'utf8');
   const fftInput = input.split('').map((element) => +element);
-  console.log(getPhaseOutput(fftInput));
+  const basePattern = ['0', '1', '0', '-1'];
+  const patterns = getAllPattern(fftInput.length, basePattern);
+  console.log(getPhaseOutput(fftInput, patterns));
 };
 
 main();
